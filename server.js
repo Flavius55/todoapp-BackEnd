a1f551d2736ad9e15cd51a5b699c7c1f07931a09
unknown <catalin_vl67@yahoo.com>
2020-07-18 23:13:05 +0300

    correct number

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(cors());
 
 //Routes
 app.post("/login", async (req,res)=>{
-    const data = await pool.query("SELECT * FROM users WHERE username = '"+req.body.username+"';");
-    // const data = await client.query("SELECT * FROM users WHERE username = '"+req.body.username+"';");
+    // const data = await pool.query("SELECT * FROM users WHERE username = '"+req.body.username+"';");
+    const data = await client.query("SELECT * FROM users WHERE username = '"+req.body.username+"';");
     if(!data.rows[0]) res.json({error:"Incorect username or password!"});
     else{
         const hashed_p = data.rows[0].hashed_p;
@@ -45,17 +45,17 @@ app.post("/login", async (req,res)=>{
 });
 
 app.post("/register" , async (req,res)=>{
-    const checkUser = await pool.query("SELECT COUNT(id) FROM users WHERE username = '"+req.body.username+"'");
-    // const checkUser = await client.query("SELECT COUNT(id) FROM users WHERE username = '"+req.body.username+"'");
+    // const checkUser = await pool.query("SELECT COUNT(id) FROM users WHERE username = '"+req.body.username+"'");
+    const checkUser = await client.query("SELECT COUNT(id) FROM users WHERE username = '"+req.body.username+"'");
     if(checkUser.rows[0].count != 0) res.json({
         error:"This username is already used"
     })
     else{
             bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
-                await pool.query(`INSERT INTO users (username,email,hashed_p) 
-                VALUES ('${req.body.username}','${req.body.email}','${hash}');`);
-                // await client.query(`INSERT INTO users (username,email,hashed_p) 
+                // await pool.query(`INSERT INTO users (username,email,hashed_p) 
                 // VALUES ('${req.body.username}','${req.body.email}','${hash}');`);
+                await client.query(`INSERT INTO users (username,email,hashed_p) 
+                VALUES ('${req.body.username}','${req.body.email}','${hash}');`);
             });
             res.json({error:false});
         }
@@ -63,16 +63,16 @@ app.post("/register" , async (req,res)=>{
 
 app.post("/addtodo" , async (req,res)=>{
     const decode = jwtDecode(req.body.token);
-    await pool.query("INSERT INTO chores (username,todo,done) VALUES ('"+decode.username+"','"+req.body.todo+"',false)");
-    // await client.query("INSERT INTO chores (username,todo,done) VALUES ('"+decode.username+"','"+req.body.todo+"',false)");
+    // await pool.query("INSERT INTO chores (username,todo,done) VALUES ('"+decode.username+"','"+req.body.todo+"',false)");
+    await client.query("INSERT INTO chores (username,todo,done) VALUES ('"+decode.username+"','"+req.body.todo+"',false)");
     console.log(req.body.todo);
     res.json(req.body.todo);
 })
 
 app.post("/deltodo" , async (req,res)=>{
     const decode = jwtDecode(req.body.token);
-    await pool.query("UPDATE chores SET done = true WHERE todo = '"+req.body.todo+"'");
-    // await client.query("UPDATE chores SET done = true WHERE todo = '"+req.body.todo+"'");
+    // await pool.query("UPDATE chores SET done = true WHERE todo = '"+req.body.todo+"'");
+    await client.query("UPDATE chores SET done = true WHERE todo = '"+req.body.todo+"'");
     res.json({error:false});
 })
 
@@ -80,9 +80,10 @@ app.post("/gettodo" , async (req,res)=>{
     if(!req.body.token) res.json({error:"invalid token"})
     else{
         const decode = jwtDecode(req.body.token);
-        const data = await pool.query("SELECT * FROM chores WHERE username = '"+decode.username+"' ORDER BY id");
-        const no = await pool.query("SELECT * FROM chores WHERE username = '"+decode.username+"' AND done = false ORDER BY id");
-        // const data = await client.query("SELECT * FROM chores WHERE username = '"+decode.username+"' ORDER BY id");
+        // const data = await pool.query("SELECT * FROM chores WHERE username = '"+decode.username+"' ORDER BY id");
+        // const no = await pool.query("SELECT * FROM chores WHERE username = '"+decode.username+"' AND done = false ORDER BY id");
+        const data = await client.query("SELECT * FROM chores WHERE username = '"+decode.username+"' ORDER BY id");
+        const no = await client.query("SELECT * FROM chores WHERE username = '"+decode.username+"' AND done = false ORDER BY id");
         res.json({
             error:false,
             data:data.rows,
